ac39854276eb898a391f2241bd81676127bb6f7f
whaffman <whaffman@student.codam.nl>
2025-09-10 17:41:04 +0200

    Add Makefile and initial implementation of MyModule with main function

---
+++ webserv/main.cpp
@@ -0,0 +1,8 @@
+#include <webserv/mymodule/MyModule.hpp>
+
+int main()
+{
+	MyModule module;
+	module.doSomething();
+	return 0;
+}
---
+++ webserv/mymodule/MyModule.cpp
@@ -0,0 +1,8 @@
+#include <webserv/mymodule/MyModule.hpp>
+
+#include <iostream>
+
+void MyModule::doSomething()
+{
+	std::cout << "Doing something!" << std::endl;
+}
