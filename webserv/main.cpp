5264139d4992442a7f13d76a197c6bf27204e2e0
whaffman <whaffman@student.codam.nl>
2025-09-22 21:49:47 +0200

    Set stdout log level to Trace for improved logging detail

--- webserv/main.cpp
+++ webserv/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
         return 1;
     }
     Log::setFileChannel("webserv.log", std::ios_base::app, Log::Level::Trace);
-    Log::setStdoutChannel(Log::Level::Info);
+    Log::setStdoutChannel(Log::Level::Trace);
     Log::info("\n======================\nStarting webserv...\n======================\n");
     Log::warning("Testing context", {{ "key1", "value1"}, {"key2", "value2"}});
     ConfigManager::getInstance().init(argv[1]); // NOLINT
