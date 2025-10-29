259b22065e6d191791277e868aa93be73883be3e
Quinten <quinten@bumbal.eu>
2025-10-29 14:30:21 +0100

    no sleep

--- htdocs/site-3/index.php
+++ htdocs/site-3/index.php
@@ -13,8 +13,6 @@ if (!isset($_SESSION['request_count'])) {
 // Increment on each request
 $_SESSION['request_count']++;
 
-sleep(8); // Simulate some processing delay
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
