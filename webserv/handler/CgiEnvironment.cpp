3f1491d47df985bace764d49cedb33c6ed7a0501
whaffman <whaffman@student.codam.nl>
2025-11-13 18:16:20 +0100

    update: cgi environment

--- webserv/handler/CgiEnvironment.cpp
+++ webserv/handler/CgiEnvironment.cpp
@@ -49,11 +49,14 @@ CgiEnvironment::CgiEnvironment(const URI &uri, const HttpRequest &request)
     env_["SERVER_PORT"] = std::to_string(port);
     env_["REMOTE_ADDR"] = request.getClient().getClientAddress(); // Placeholder, should be set to actual remote address
     env_["REDIRECT_STATUS"] = "200";                              // Required by PHP with force-cgi-redirect enabled
-    env_["SERVER_SOFTWARE"] = "Webserv/1.0";
+    env_["SERVER_SOFTWARE"] = "Webserv/1.2";
     env_["REQUEST_SCHEME"] = "HTTP";
     env_["HTTP_VERSION"] = "1.1";
     // Add HTTP_ headers
     const HttpHeaders &headers = request.getHeaders();
+
+    env_["HTTP_HOST"] = headers.getHost().value_or("localhost:8080"); // TODO: Default value for testing
+    env_["BLALALAL"] = "ASD";
     env_["HTTP_COOKIE"] = headers.get("Cookie");
     env_["HTTP_USER_AGENT"] = headers.get("User-Agent");
     env_["HTTP_ACCEPT"] = headers.get("Accept");
